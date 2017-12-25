2347e13ea0c5e2f2be5a6f3d0a05cee0aac55851
Yomaira Escano <yjescano@gmail.com>
2017-12-25 02:28:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(name, grade)
+  def initialize(name, grade, id=nil)
     @name = name
     @grade = grade
     # @id = id
