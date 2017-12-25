486d3df5a3b730bdb8a8d7522c926a3efcc1cb58
Yomaira Escano <yjescano@gmail.com>
2017-12-25 02:19:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   def initialize(name:, grade:, id=nil)
     @name = name
     @grade = grade
-    @id = id
+    # @id = id
   end
 
   # Remember, you can access your database connection anywhere in this class
